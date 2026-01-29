578cdec7dcfb960eb627ee4746f32cbd9c63cc2a
zofiastasiak <aisoz.2004@wp.pl>
2026-01-29 01:57:08 +0100

    poprawiony wykres zdrowie psychiczne

---
+++ Untitled.R
@@ -0,0 +1,8 @@
+install.packages("ggstatsplot")
+install.packages("gtsummary")
+install.packages
+install.packages("knitr")
+install.packages("kableExtra")
+install.packages("psych")
+install.packages("summarytools")
+install.packages("gtsummary")
